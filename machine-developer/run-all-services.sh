4ba5cbe87a339d7cfe78d950817cff5a69e0591b
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-07-26 12:02:53 +0200

    Add verbose mode and exit on any command fail in run-all-services.sh

--- machine-developer/run-all-services.sh
+++ machine-developer/run-all-services.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+# Turn verbose mode on
 set -x
+# Exit when any command fails
+set -e
+
 REPOS_DESTINATION="${REPOS_DESTINATION:=~/repos/autocoin}"
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-auth-service.yml
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-frontend.yml
