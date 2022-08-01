07d3d712d04065f704c1bede2fa8ec9a3be7188f
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-08-01 17:27:32 +0200

    Add starting exchange-mediator and balance-monitor

--- machine-developer/run-all-services.sh
+++ machine-developer/run-all-services.sh
@@ -6,4 +6,6 @@ set -e
 
 REPOS_DESTINATION="${REPOS_DESTINATION:=~/repos/autocoin}"
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-auth-service.yml
+ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-exchange-mediator.yml
+ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-balance-monitor.yml
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-frontend.yml
