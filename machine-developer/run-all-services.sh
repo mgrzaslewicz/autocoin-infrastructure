999e1a6384a48ad9a578e3f5692712c95ab6e578
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-04 20:11:23 +0100

    Add arbitrage-monitor start playbook

--- machine-developer/run-all-services.sh
+++ machine-developer/run-all-services.sh
@@ -8,4 +8,5 @@ REPOS_DESTINATION="${REPOS_DESTINATION:=~/repos/autocoin}"
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-auth-service.yml
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-exchange-mediator.yml
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-balance-monitor.yml
+ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-arbitrage-monitor.yml
 ansible-playbook --extra-vars="repos_destination=${REPOS_DESTINATION}" run/run-frontend.yml
